de95c77fdb78903c7047b5b41ee3fa272789ed87
Nicolas <00001118949626sp@al.educacao.sp.gov.br>
2024-05-07 18:16:18 -0300

    python

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+def soma (a,b);
+  return a+b
+
+if __name__ == "__main__":
+  num1 = float(imput("digite o número: "))
+  num2 = float(imput("digite o segundo número: "))
+  resultado = soma(num1, num2)
+  
+  print(f"resultado: {resultado}")  
+  
